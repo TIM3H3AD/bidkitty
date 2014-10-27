4242734ee712bc0d1703da72780ab50b2c6009bf
ethascloy <whalebone.earth@gmail.com>
2014-10-27 00:41:21 -0400

    .03

---
+++ app/controllers/static_pages_controller.rb
@@ -0,0 +1,19 @@
+class StaticPagesController < ApplicationController
+  
+  def about
+  	  render
+  end
+  
+  def support
+  	render
+  end
+  
+  def proxy_party
+  	render  
+  end
+
+  def home
+  	  render
+  end
+  
+end
