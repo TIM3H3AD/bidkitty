c96ea727f39cc8bc9ceed84dc64398c1b289ab58
ethascloy <whalebone.earth@gmail.com>
2014-10-30 20:40:59 -0400

    .04

--- app/models/bid.rb
+++ app/models/bid.rb
@@ -1,3 +1,3 @@
 class Bid < ActiveRecord::Base
-  validates :auction_end, :auction_id, :bid_threshold, :maker, :maker_phone, :plan, :presence => true
+  validates :maker, :maker_phone, :plan, :presence => true
 end
