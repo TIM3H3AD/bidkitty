aa77512080203c99ceec504c35f8c4ee3fea389f
ethascloy <whalebone.earth@gmail.com>
2015-09-06 18:40:09 -0400

    TDD

--- app/controllers/bids_controller.rb
+++ app/controllers/bids_controller.rb
@@ -1,7 +1,7 @@
 class BidsController < ApplicationController
-  http_basic_authenticate_with name: "whalebone", password: "theonlyescape", only: :index :delete
+  http_basic_authenticate_with name: "whalebone", password: "theonlyescape", only: :index, :delete
   
-  http_basic_authenticate_with name: "turtledove", password: "onelove", only: :index :edit
+  http_basic_authenticate_with name: "turtledove", password: "onelove", only: :index, :edit
   
 def new
 @bid = Bid.new
