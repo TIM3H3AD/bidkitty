16271f0bd462354a43dc9dbbcca4c46d73245c65
ethascloy <whalebone.earth@gmail.com>
2015-09-14 08:35:45 -0400

    TDD

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,7 @@ Rails.application.routes.draw do
   root to: 'static_pages#home'
   
   get '/about' => 'static_pages#about'
+  get '/crowdfund' => 'static_pages#crowdfund'
   get '/mgmt' => 'static_pages#mgmt'
   get '/wb' => 'static_pages#wb'
   get '/support' => 'static_pages#support'
